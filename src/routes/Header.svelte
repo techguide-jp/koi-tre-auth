4547832b2c55cf5b9e9d890625e96ff01f2c21f7
Yuta Takahashi <m1205123ing@gmail.com>
2024-05-14 19:49:31 +0900

    header footer 調整

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -37,14 +37,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/counter' ? 'page' : undefined}>
-				<a href="/counter">Counter</a>
-			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
 				<a href="/contact">Contact</a>
